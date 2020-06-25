db7c3a5f0a025ec42a8fcc873eb8057942ab5ca8
Alvaro Centeno <alvaroc0689@gmail.com>
2020-06-24 21:46:42 -0700

    built homepage that shows mysql to page

---
+++ client/assets/edit.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams.get('id'));
+
+})
--- client/assets/home.js
+++ client/assets/home.js
@@ -4,8 +4,43 @@ $(document).ready(function () {
         type: 'GET',
         url: '/api',
 
-    }).then((res) => {
-        console.log(res);
+    }).then((allTodos) => {
+        console.log(allTodos);
+        renderTodos(allTodos);
     })
-
 })
+
+const renderTodos = (arr) => {
+    $('#card-container').html('');
+    arr.forEach((todo) => {
+        console.log(todo);
+
+        let msg = todo.completed ? ",/ comleted" : 'X Need to do!';
+
+        $('#card-container').prepend(
+            `
+<div class="card mb-2">
+                    <div class="card-body">
+                        <h6 class="card-subtitle mb-2 text-muted"> ✔ ${msg}</h6>
+                        <p class="card-text">
+                        ${todo.text}</p>
+                        <div class="text-center">
+                            <button  data-id=${todo.id} style="width: 150px" class="btn btn-outline-success btnUpdate">Edit</button>
+                            <button  data-id=${todo.id} style="width: 150px" class="btn btn-outline-danger btnDelete">Delete</button>
+                        </div>
+                    </div>
+                </div>
+`
+        );
+    })
+};
+
+$(document).on('click', '.btnUpdate', function () {
+    const todoId = $(this).attr('data-id');
+    window.location.href = `/edit?id=${todoId}`;
+});
+
+$(document).on('click', '.btnDelete', function () {
+    const todoId = $(this).attr('data-id');
+    window.location.href = `/delete?id=${todoId}`;
+});
